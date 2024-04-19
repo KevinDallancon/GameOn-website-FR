9e3267c6de7c7896b963c5a1f18d358eaf4f923f
Kévin Dallançon <kevin.dallancon@hotmail.fr>
2024-04-19 17:05:45 +0200

    Ajout message error radio

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,131 +31,151 @@ closeModal.addEventListener('click', () => {
 
 
 
-function verifierChamp(balise) {
-  const formDataParent = balise.closest('.formData');
+function verifierPrenomNom(balise) {
 
-  if (balise.value === "") {
-    formDataParent.dataset.error = "Le champ ne doit pas être vide";
-    formDataParent.dataset.errorVisible = "true";
-  } else if (balise.value.length < 2) {
-    formDataParent.dataset.error = "Le champ doit contenir au moins deux caractères";
-    formDataParent.dataset.errorVisible = "true";
-  } else {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
-  }
+// Définition du RegExp
+let nameRegExp = new RegExp('^[A-Za-z-]{2,30}$', 'g');
+// Test du RegExp
+let testName = nameRegExp.test(balise.value);
+// Résultat conditionnel
+if (testName) {
+  balise.nextElementSibling.innerText = "Champ valide.";
+  balise.nextElementSibling.style.color = "green";
+  return true;
+} else {
+  balise.nextElementSibling.innerText = "Champ vide ou incorrect.";
+  balise.nextElementSibling.style.color = "red";
+  return false;
+};
 }
 
 function verifierEmail(balise) {
-  const formDataParent = balise.closest('.formData');
-
-  const regexMail = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
-
-  if ( regexMail.test(balise.value)) {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
-    
-  } else {
-    
-    formDataParent.dataset.error = "Veuillez entrer une adresse email valide.";
-    formDataParent.dataset.errorVisible = "true";
-  }
-}
-function verifierBirthdate(balise) {
   
-  const formDataParent = balise.closest('.formData');
-  // Vérifie si une date a été entrée
-  if (!balise.value) {
-    formDataParent.dataset.error = "Veuillez entrer votre date de naissance.";
-    formDataParent.dataset.errorVisible = "true";
+  // Définition du regexMail
+  let regexMail = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+  // Test du regexMail
+  let testMail = regexMail.test(balise.value)
+
+  if (testMail) {
+    balise.nextElementSibling.innerText = "Champ valide.";
+    balise.nextElementSibling.style.color = "green";
+    return true;
   } else {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
+    balise.nextElementSibling.innerText = "Champ vide ou incorrect.";
+    balise.nextElementSibling.style.color = "red";
+    return false;
   }
-
 }
+// function verifierBirthdate(balise) {
 
+//   const formDataParent = balise.closest('.formData');
+//   const dateFormat = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+//   // Vérifie si une date a été entrée
+//   if (!balise.value) {
+//     formDataParent.dataset.error = "Veuillez entrer votre date de naissance.";
+//     formDataParent.dataset.errorVisible = "true";
+//   } else if (!dateFormat.test(balise.value)) {
+//     formDataParent.dataset.error = "Cette date de naissance semble incorrecte. Assurez-vous d'utiliser le format JJ/MM/AAAA";
+//     formDataParent.dataset.errorVisible = "true";
+//   } else {
+//     formDataParent.dataset.error = "";
+//     formDataParent.dataset.errorVisible = "false";
+//   }
+
+// }
 
 function verifierChampNum(balise) {
 
-  const formDataParent = balise.closest('.formData');
   // Expression régulière qui accepte uniquement des chiffres
-  const regex = /^\d+$/;
+  let regexChampNum = /^\d+$/;
+  // Test du regex
+  let testRegexChamp = regexChampNum.test(balise.value);
 
-  if (regex.test(balise.value)) {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
+  if (testRegexChamp) {
+    balise.nextElementSibling.innerText = "Champ valide.";
+    balise.nextElementSibling.style.color = "green";
+    return true;
   } else {
-    formDataParent.dataset.error = "Veuillez entrer un nombre valide. Par exemple, 0, 1, 2, etc. ";
-    formDataParent.dataset.errorVisible = "true";
+    balise.nextElementSibling.innerText = "Champ vide ou incorrect.";
+    balise.nextElementSibling.style.color = "red";
+    return false;
   }
 }
 
 function verifierRadio(balise) {
 
-  const formDataParent = balise[0].closest('.formData');
   let etatBtn = false;
-  
+  let errorElement = document.getElementById('error-radio'); 
+  console.log(errorElement);
+
   for (let radio of balise) {
     if (radio.checked) {
+      console.log(radio);
       etatBtn = true;
       break;
     }
-  }
+  };
 
   if (etatBtn) {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
+    console.log(etatBtn);
+    errorElement.innerText = "Champ valide.";
+    errorElement.style.color = "green";
+    return true;
   } else {
-    formDataParent.dataset.error = "Vous devez choisir une option.";
-    formDataParent.dataset.errorVisible = "true";
+    errorElement.innerText = "Champ vide ou incorrect.";
+    errorElement.style.color = "red";
+    return false;
   }
 }
 function verifierCg(balise) {
 
-  const formDataParent = balise.closest('.formData');
-
   if (balise.checked)
   {
-    formDataParent.dataset.error = "";
-    formDataParent.dataset.errorVisible = "false";
+    balise.nextElementSibling.innerText = "Champ valide.";
+    balise.nextElementSibling.style.color = "green";
+    return true;
   } else {
-    formDataParent.dataset.error = "Vous devez vérifier que vous acceptez les termes et conditions.";
-    formDataParent.dataset.errorVisible = "true";
+    balise.nextElementSibling.innerText = "Champ vide ou incorrect.";
+    balise.nextElementSibling.style.color = "red";
+    return false;
   }
 }
 
 // Ajout d'un ecouteur d'évenement pour l'evenement Submit
 form.addEventListener('submit', (event) => {
 
-  try {
-    // On empeche le comportement par default
-    event.preventDefault();
-
-    const balisePrenom = document.getElementById('first');
-    verifierChamp(balisePrenom);
-
-    const baliseNom = document.getElementById('last');
-    verifierChamp(baliseNom);
-
-    const baliseMail = document.getElementById('email');
-    verifierEmail(baliseMail);
-
-    const balisebirthdate = document.getElementById('birthdate');
-    verifierBirthdate(balisebirthdate);
-
-    const baliseTournois = document.getElementById('quantity');
-    verifierChampNum(baliseTournois);
-
-    const baliseRadio = document.querySelectorAll('input[type="radio"]');
-    verifierRadio(baliseRadio);
-
-    const baliseCg = document.getElementById('checkbox1');
-    verifierCg(baliseCg);
-
-  } catch (error) {
-    console.log("Une erreur est survenue : " + error.message);
+  function validate() {
+    try {
+      // On empeche le comportement par default
+      event.preventDefault();
+  
+      const balisePrenom = document.getElementById('first');
+      verifierPrenomNom(balisePrenom);
+  
+      const baliseNom = document.getElementById('last');
+      verifierPrenomNom(baliseNom);
+  
+      const baliseMail = document.getElementById('email');
+      verifierEmail(baliseMail);
+  
+      // const balisebirthdate = document.getElementById('birthdate');
+      // verifierBirthdate(balisebirthdate);
+  
+      const baliseTournois = document.getElementById('quantity');
+      verifierChampNum(baliseTournois);
+  
+      const baliseRadio = document.querySelectorAll('input[type="radio"]');
+      verifierRadio(baliseRadio);
+  
+      const baliseCg = document.getElementById('checkbox1');
+      verifierCg(baliseCg);
+  
+  
+    } catch (error) {
+      console.log("Une erreur est survenue : " + error.message);
+    }
+  
   }
-
+  validate()
 
 })
